023f5f90fad2ba487e144fa1556e0b3d714de2a0
majesty-01 <moheethasan4@gmail.com>
2023-05-02 23:11:32 +0600

    created chefs data

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require("./data/chefsData.json");
+
 app.get("/", (req, res) => {
   res.send("chef is cooking");
 });
