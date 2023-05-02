13951023342828e1ed9b5a531bfb5a71c375c44c
majesty-01 <moheethasan4@gmail.com>
2023-05-02 23:51:18 +0600

    sent chefs data to client side and updated chefs picture

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 const chefs = require("./data/chefsData.json");
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("chef is cooking");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
